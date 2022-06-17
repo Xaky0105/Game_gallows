c418b9bc144f47e65d21a6be669b64990855fd1b
Timahvey12 <energye@tut.by>
2022-06-17 21:12:15 +0300

    Function generator word

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const arrWords = ['стакан', 'диван', 'матрас',  'палас', 'мотор'];
 
 function generateWord (hidenWord) {
-  
+  const num = Math.floor(Math.random() * hidenWord.length);
+  return  hidenWord[num];
+
 }
+console.log(generateWord(arrWords));
